63663aa61a9e28db258fa76cbd5a1719bebed8ea
Ann Blodgett <w.annblodgett@gmail.com>
2019-12-02 18:53:36 -0700

    Add files via upload

--- adventOfCode/adventofcode_2019_day_1.cpp
+++ adventOfCode/adventofcode_2019_day_1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>  
+#include <fstream>
+using namespace std;
 
   int fuelCounterUpper(int mass){
     int result;
@@ -8,9 +10,28 @@
 
 int main() {  
 
-std::cout<< fuelCounterUpper(12)<<std::endl;
-std::cout<< fuelCounterUpper(14)<<std::endl;
-std::cout<< fuelCounterUpper(1969)<<std::endl;
-std::cout<< fuelCounterUpper(100756)<<std::endl;
+int sum = 0;
+int x;
+
+ifstream numbersfile;
+numbersfile.open("numbers.txt");
+
+
+if(!numbersfile.is_open()){
+    cout<<"File failed to open"<<endl;
+    return 0;
+}
+
+while (numbersfile>>x){
+	//std::cout<<"The number read from the file is: "<<x<< endl;
+  int fuelForModule;
+  fuelForModule = fuelCounterUpper(x);
+  //std::cout<<"The fuel needed for this module is: "<<fuelForModule<<std::endl;
+   sum = sum+fuelForModule;
+  // std::cout<<"The sum of the total fuel needed is now : "<<sum<<std::endl; 
+}
+
+cout<<"The total fuel needed is: "<<sum<<endl;
+
   return 0;
 }
